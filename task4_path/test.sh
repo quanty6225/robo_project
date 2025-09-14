b5b3f0b616299f56773f8b36fa5f800220320908
quanty6225 <2502013337@qq.com>
2025-09-14 22:26:11 +0800

    修改test4

--- task4_path/test.sh
+++ task4_path/test.sh
@@ -2,20 +2,20 @@
 
 g++ -std=c++11 -Wall -o path_planner path_planner.cpp
 
-echo "运行路径规划测试 (从 (0,0) 到 (1,0))..."
-./path_planner "map.txt" 0 0 1 0
 echo "运行路径规划测试 (从 (0,0) 到 (2,0))..."
 ./path_planner "map.txt" 0 0 2 0
 echo "运行路径规划测试 (从 (0,0) 到 (3,0))..."
 ./path_planner "map.txt" 0 0 3 0
+echo "运行路径规划测试 (从 (0,0) 到 (4,0))..."
+./path_planner "map.txt" 0 0 4 0
 echo "运行路径规划测试 (从 (0,0) 到 (0,1))..."
 ./path_planner "map.txt" 0 0 0 1
-echo "运行路径规划测试 (从 (0,0) 到 (0,2))..."
-./path_planner "map.txt" 0 0 0 2
-echo "运行路径规划测试 (从 (0,0) 到 (1,1))..."
-./path_planner "map.txt" 0 0 1 1
-echo "运行路径规划测试 (从 (0,0) 到 (4,4))..."
-./path_planner "map.txt" 0 0 4 4
 echo "运行路径规划测试 (从 (0,0) 到 (0,-1))..."
 ./path_planner "map.txt" 0 0 0 -1
+echo "运行路径规划测试 (从 (4,0) 到 (4,1))..."
+./path_planner "map.txt" 4 0 4 1
+echo "运行路径规划测试 (从 (4,0) 到 (0,4))..."
+./path_planner "map.txt" 4 0 0 4
+echo "运行路径规划测试 (从 (4,0) 到 (2,3))..."
+./path_planner "map.txt" 4 0 2 3
 
