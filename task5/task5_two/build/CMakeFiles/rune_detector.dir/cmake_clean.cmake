3b887ab13d2667c53d7f1d419419f70278b938ef
quanty6225 <2502013337@qq.com>
2025-09-14 14:41:16 +0800

    task5.2

---
+++ task5/task5_two/build/CMakeFiles/rune_detector.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rune_detector.dir/rune_detector.cpp.o"
+  "CMakeFiles/rune_detector.dir/rune_detector.cpp.o.d"
+  "rune_detector"
+  "rune_detector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rune_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
